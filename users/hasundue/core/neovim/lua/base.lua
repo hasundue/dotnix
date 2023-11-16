48e13c805491bfb953f70e31dfa61b0b2e8d8f3d
hasundue <hasundue@gmail.com>
2023-11-16 09:42:13 +0900

    fix(neovim): adopt to API change in clipboard

--- users/hasundue/core/neovim/lua/base.lua
+++ users/hasundue/core/neovim/lua/base.lua
@@ -10,12 +10,12 @@ local wo = vim.wo
 g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.clipboard.osc52').copy,
-    ['*'] = require('vim.clipboard.osc52').copy,
+    ['+'] = require('vim.ui.clipboard.osc52').copy,
+    ['*'] = require('vim.ui.clipboard.osc52').copy,
   },
   paste = {
-    ['+'] = require('vim.clipboard.osc52').paste,
-    ['*'] = require('vim.clipboard.osc52').paste,
+    ['+'] = require('vim.ui.clipboard.osc52').paste,
+    ['*'] = require('vim.ui.clipboard.osc52').paste,
   },
 }
 opt.clipboard = "unnamedplus"
