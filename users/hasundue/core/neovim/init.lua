3f03fbaa1fafa4e8a17213b3190cb11cfd5e89b2
hasundue <hasundue@gmail.com>
2023-11-19 12:04:57 +0900

    perf(neovim): disable unused default plugins (not working fsr)

--- users/hasundue/core/neovim/init.lua
+++ users/hasundue/core/neovim/init.lua
@@ -1,8 +1,11 @@
 -- enable experimental lua loader
 vim.loader.enable()
 
+-- disable default plugins
+require("rc.builtin")
+
 -- base configuration, independent of plugins (keymaps, etc)
 require("base")
 
 -- load plugins with dpp.vim
-vim.cmd("source ~/.config/nvim/rc/dpp/init.vim")
+vim.cmd("source " .. vim.fn.stdpath("config") .. "/rc/dpp/init.vim")
