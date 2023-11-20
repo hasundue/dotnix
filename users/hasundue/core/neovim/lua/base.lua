908f59c5c95e93100130919b59803c19969330c0
hasundue <hasundue@gmail.com>
2023-11-20 16:20:23 +0900

    fix(neovim): fix clipboard config

--- users/hasundue/core/neovim/lua/base.lua
+++ users/hasundue/core/neovim/lua/base.lua
@@ -4,15 +4,17 @@ local opt = vim.opt
 --
 -- clipboard -------------------------------------
 --
+local osc52 = require('vim.ui.clipboard.osc52')
+
 g.clipboard = {
   name = 'OSC 52',
   copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy,
-    ['*'] = require('vim.ui.clipboard.osc52').copy,
+    ['+'] = osc52.copy('+'),
+    ['*'] = osc52.copy('*'),
   },
   paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste,
-    ['*'] = require('vim.ui.clipboard.osc52').paste,
+    ['+'] = osc52.paste('+'),
+    ['*'] = osc52.paste('*'),
   },
 }
 opt.clipboard = "unnamedplus"
