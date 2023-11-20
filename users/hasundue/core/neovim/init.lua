ee8da4ca9ad72aa255d4254acd5c38969a946fb3
hasundue <hasundue@gmail.com>
2023-11-20 17:49:55 +0900

    refactor(neovim): mv `base.lua` to `rc/base.lua`

--- users/hasundue/core/neovim/init.lua
+++ users/hasundue/core/neovim/init.lua
@@ -2,7 +2,7 @@
 vim.loader.enable()
 
 -- base configuration, independent of plugins (keymaps, etc)
-require("base")
+require("rc.base")
 
 -- load plugins with dpp.vim
 vim.cmd("source " .. vim.fn.stdpath("config") .. "/rc/dpp/init.vim")
