d4216a5e1e1ea04cc4c6f5d8c040be0a8f35bfd3
hasundue <hasundue@gmail.com>
2023-11-20 16:20:49 +0900

    refactor(neovim): simplify `rc.terminal`

--- users/hasundue/core/neovim/lua/rc/terminal.lua
+++ users/hasundue/core/neovim/lua/rc/terminal.lua
@@ -1,13 +1,11 @@
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 
-local function config_term()
-  vim.opt_local.termguicolors = true
-  vim.opt_local.number = false
-  vim.opt_local.signcolumn = 'no'
-  vim.opt_local.relativenumber = false
-end
-
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
   pattern = { "*" },
-  callback = config_term,
+  callback = function()
+    vim.opt_local.termguicolors = true
+    vim.opt_local.number = false
+    vim.opt_local.signcolumn = 'no'
+    vim.opt_local.relativenumber = false
+  end
 })
