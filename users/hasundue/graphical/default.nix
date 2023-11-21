8ad47b3d4c03ecf8ecf482b5d33232b2f2cf5896
hasundue <hasundue@gmail.com>
2023-11-21 17:34:31 +0900

    refactor(user/graphical): use system fonts

--- users/hasundue/graphical/default.nix
+++ users/hasundue/graphical/default.nix
@@ -62,24 +62,6 @@
   };
 
   stylix = {
-    fonts = {
-      sansSerif = {
-        package = pkgs.noto-fonts-cjk-sans;
-        name = "Noto Sans CJK JP";
-      };
-      serif = {
-        package = pkgs.noto-fonts-cjk-serif;
-        name = "Noto Serif CJK JP";
-      };
-      monospace = {
-        package = pkgs.nerdfonts.override { fonts = [ "Hack" ]; };
-        name = "Hack Nerd Font";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
     image = pkgs.fetchurl {
       url = "https://upload.wikimedia.org/wikipedia/commons/a/a5/Tsunami_by_hokusai_19th_century.jpg";
       hash = "sha256-MMFwJg3jk/Ub1ZKtrMbwcUtg8VfAl0TpxbbUbmphNxg=";
