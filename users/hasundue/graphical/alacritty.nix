dad3ff4770242bc8d307f2d4eb117d182f13167b
hasundue <hasundue@gmail.com>
2023-11-21 22:24:42 +0900

    feat(alacritty): add padding

--- users/hasundue/graphical/alacritty.nix
+++ users/hasundue/graphical/alacritty.nix
@@ -3,6 +3,9 @@
     enable = true;
     settings = {
       decorations = "none";
+      font = {
+        offset = { x = 1; y = 1; };
+      };
       window = {
         padding = { x = 10; y = 10; };
         dynamic_padding = true;
