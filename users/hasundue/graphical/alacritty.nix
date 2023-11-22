2423ad48d94b622b154ccdcba5a147d35d75a443
hasundue <hasundue@gmail.com>
2023-11-22 10:32:02 +0900

    revert: "feat(alacritty): add padding"
    This reverts commit dad3ff4770242bc8d307f2d4eb117d182f13167b.

--- users/hasundue/graphical/alacritty.nix
+++ users/hasundue/graphical/alacritty.nix
@@ -3,9 +3,6 @@
     enable = true;
     settings = {
       decorations = "none";
-      font = {
-        offset = { x = 1; y = 1; };
-      };
       window = {
         padding = { x = 10; y = 10; };
         dynamic_padding = true;
