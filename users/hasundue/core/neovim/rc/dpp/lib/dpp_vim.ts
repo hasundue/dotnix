1d86a8f5feb5c746dbd015aeed8dc8c4227f4ab0
hasundue <hasundue@gmail.com>
2023-12-02 18:02:33 +0900

    build(deps): update flakes

--- users/hasundue/core/neovim/rc/dpp/lib/dpp_vim.ts
+++ users/hasundue/core/neovim/rc/dpp/lib/dpp_vim.ts
@@ -4,5 +4,5 @@ export {
   ContextBuilder,
   Dpp,
   type Plugin,
-} from "https://deno.land/x/dpp_vim@v0.0.7/types.ts";
-export type { Denops } from "https://deno.land/x/dpp_vim@v0.0.7/deps.ts";
+} from "https://deno.land/x/dpp_vim@v0.0.8/types.ts";
+export type { Denops } from "https://deno.land/x/dpp_vim@v0.0.8/deps.ts";
