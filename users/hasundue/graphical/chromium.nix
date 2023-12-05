1e136dc2b0e5d89822bc51174230983f56f0ab51
hasundue <hasundue@gmail.com>
2023-12-05 17:50:50 +0900

    feat(chromium): add nos2x

--- users/hasundue/graphical/chromium.nix
+++ users/hasundue/graphical/chromium.nix
@@ -5,6 +5,7 @@
       { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
       { id = "fihnjjcciajhdojfnbdddfaoknhalnja"; } # I don't care about cookies
+      { id = "kpgefcfmnafjgpblomihpgmejjdanjjp"; } # nos2x
       { id = "fjdmkanbdloodhegphphhklnjfngoffa"; } # YouTube Auto HD
     ];
   };
