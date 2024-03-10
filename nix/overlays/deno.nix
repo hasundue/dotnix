77a6afc56d24543bb9333909f316e761839e26cf
hasundue <hasundue@gmail.com>
2024-03-10 22:41:20 +0900

    feat(nix/overlays): add deno overlay

--- nix/overlays/deno.nix
+++ nix/overlays/deno.nix
@@ -1,2 +1,18 @@
-final: prev: {
+final: prev: 
+{
+  deno = prev.deno.override (oldAttrs: {
+    rustPlatform.buildRustPackage = args:
+      final.rustPlatform.buildRustPackage (
+        args // rec {
+          version = "1.41.2";
+          src = final.fetchFromGitHub {
+            owner = "denoland";
+            repo = args.pname;
+            rev = "v${version}";
+            hash = "sha256-l8He7EM9n8r7OTC6jN6F8ldf3INXxEeaUI1u6AfR7RI=";
+          };
+          cargoHash = "sha256-T+6b4bGx7y/7E0CIacKFQ32DCAiNFXFi15ibq7rDfI4=";
+        }
+      );
+  });
 }
