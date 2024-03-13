adcd363090e8c8eae70f5b73605442e7deaa5dc2
hasundue <hasundue@gmail.com>
2024-03-13 14:27:16 +0900

    fix(waybar): widen gap in battery icon and text

--- users/hasundue/graphical/waybar.nix
+++ users/hasundue/graphical/waybar.nix
@@ -94,10 +94,10 @@
           warning = 30;
           critical = 15;
         };
-        format = "{icon}  {capacity}%";
+        format = "{icon}   {capacity}%";
         format-charging = "󰂄  {capacity}%";
         format-plugged = "   {capacity}%";
-        format-alt = "{icon}  {time}";
+        format-alt = "{icon}   {time}";
         format-icons = [ " " " " " " " " " " ];
       };
 
