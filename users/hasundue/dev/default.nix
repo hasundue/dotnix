d16a135d87df2e49fdf59187b298aaff7ad97bc9
hasundue <hasundue@gmail.com>
2024-05-12 13:31:48 +0900

    feat(lazygit): wider sidepanel

--- users/hasundue/dev/default.nix
+++ users/hasundue/dev/default.nix
@@ -25,4 +25,13 @@
     enable = true;
     keyConfig = builtins.readFile ./gitui.ron;
   };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        sidePanelWidth = 0.5;
+      };
+    };
+  };
 }
