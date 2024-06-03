c7c23dc57e67773d9a9f78740d4073f36014c3d6
hasundue <hasundue@gmail.com>
2024-06-04 08:50:53 +0900

    chore: "rollback" to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -11,12 +11,12 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
