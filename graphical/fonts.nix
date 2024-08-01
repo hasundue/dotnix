d195a60024b9502901d55e18863c87de3d8ac343
hasundue <hasundue@gmail.com>
2024-08-01 16:54:31 +0900

    feat(fonts): increase font size from 11 to 12

--- graphical/fonts.nix
+++ graphical/fonts.nix
@@ -57,10 +57,10 @@
       name = "Noto Color Emoji";
     };
     sizes = {
-      applications = 11;
-      desktop = 11;
-      popups = 11;
-      terminal = 11;
+      applications = 12;
+      desktop = 12;
+      popups = 12;
+      terminal = 12;
     };
   };
 }
