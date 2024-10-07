a900736f9fac2fc0d79f5ad4efb0bf5409ea1d45
hasundue <hasundue@gmail.com>
2024-10-07 13:50:32 +0900

    fix(sway): update fn keys to use WirePlumber

--- users/hasundue/graphical/sway.nix
+++ users/hasundue/graphical/sway.nix
@@ -13,24 +13,26 @@
         smartGaps = false;
         inner = 5;
       };
-      keybindings = let
-        mod = config.wayland.windowManager.sway.config.modifier;
-      in lib.mkOptionDefault {
-        # Function Keys
-        "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
-        "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
-        "XF86AudioMute" = "exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
-        "XF86AudioMicMute" = "exec pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-        "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
-        "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
-        # Screenshots
-        "Print" = "exec grimshot savecopy output";
-        "Alt+Print" = "exec grimshot savecopy active";
-        "Ctrl+Print" = "exec grimshot savecopy area";
-        # Power Management
-        "${mod}+Ctrl+l" = "exec swaylock -fF";
-        "${mod}+Ctrl+s" = "exec systemctl suspend";
-      };
+      keybindings =
+        let
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+        lib.mkOptionDefault {
+          # Function Keys
+          "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
+          "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+          "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          "XF86AudioMicMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+          "XF86MonBrightnessUp" = "exec brightnessctl set +5%";
+          "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
+          # Screenshots
+          "Print" = "exec grimshot savecopy output";
+          "Alt+Print" = "exec grimshot savecopy active";
+          "Ctrl+Print" = "exec grimshot savecopy area";
+          # Power Management
+          "${mod}+Ctrl+l" = "exec swaylock -fF";
+          "${mod}+Ctrl+s" = "exec systemctl suspend";
+        };
       menu = "wofi --show run";
       terminal = lib.getExe pkgs.alacritty;
       window = {
