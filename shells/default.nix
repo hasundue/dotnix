32b04125cb391c58ee402436f476c94f0cc086c9
Shun Ueda <hasundue@gmail.com>
2024-11-30 15:05:49 +0000

    fix(shells): make aliases available for direnv

--- shells/default.nix
+++ shells/default.nix
@@ -1,6 +1,19 @@
 { pkgs, lib, ... } @ args:
 
-lib.mapAttrs (name: path: pkgs.mkShell ((import path) args)) rec {
+let
+  mkScript = alias: command:
+    pkgs.writeShellScriptBin alias command;
+
+  mkShell =
+    { aliases ? { }
+    , packages ? [ ]
+    ,
+    }:
+    pkgs.mkShell {
+      packages = packages ++ (lib.mapAttrsToList mkScript aliases);
+    };
+in
+lib.mapAttrs (name: path: mkShell ((import path) args)) rec {
   deno = ./deno.nix;
   nix = ./nix.nix;
   default = nix;
--- shells/nix.nix
+++ shells/nix.nix
@@ -1,22 +1,6 @@
-{ pkgs, lib, neovim-flake, ... }:
+{ pkgs, neovim-flake, ... }:
 
 let
-  aliases = lib.mapAttrsToList
-    (name: value: "alias ${name}='${value}'")
-    rec {
-      nf = "nix flake";
-      nfc = "${nf} check";
-      nfs = "${nf} show";
-      nfu = "${nf} update";
-
-      nr = "sudo nixos-rebuild --flake .";
-      nrb = "${nr} boot |& nom";
-      nrs = "${nr} switch |& nom";
-      nrt = "${nr} test |& nom";
-
-      nv = "nvim";
-    };
-
   neovim = neovim-flake.neovim {
     modules = with neovim-flake; [
       core
@@ -27,11 +11,23 @@ let
   };
 in
 {
+  aliases = rec {
+    nf = "nix flake";
+    nfc = "${nf} check";
+    nfs = "${nf} show";
+    nfu = "${nf} update";
+
+    nr = "sudo nixos-rebuild --flake .";
+    nrb = "${nr} boot |& nom";
+    nrs = "${nr} switch |& nom";
+    nrt = "${nr} test |& nom";
+
+    nv = "nvim";
+  };
+
   packages = with pkgs; [
     cachix
     neovim
     nix-output-monitor
   ];
-
-  shellHook = lib.concatStringsSep "\n" aliases;
 }
