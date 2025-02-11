b69cfebdb5eccfda1de1a8b4ed046549ac374f60
Shun Ueda <hasundue@gmail.com>
2025-02-11 12:46:48 +0900

    shells/ebiten: init

--- shells/default.nix
+++ shells/default.nix
@@ -7,15 +7,19 @@ let
     '';
   mkShell =
     { aliases ? { }
+    , nativeBuildInputs ? [ ]
     , packages ? [ ]
+    , shellHook ? ""
     ,
     }:
-    pkgs.mkShellNoCC {
+    pkgs.mkShell {
+      inherit nativeBuildInputs shellHook;
       packages = packages ++ (lib.mapAttrsToList mkScript aliases);
     };
 in
 lib.mapAttrs (name: path: mkShell ((import path) args)) rec {
   deno = ./deno.nix;
+  ebiten = ./ebiten.nix;
   nix = ./nix.nix;
   default = nix;
 }
