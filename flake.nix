3df23b24a45cfbf02c944d840b184a4187437ee4
Shun Ueda <hasundue@gmail.com>
2025-02-17 10:23:21 +0900

    flake: update

--- flake.nix
+++ flake.nix
@@ -70,7 +70,11 @@
       nixosSystem = system: host: forSystem system (args: lib.nixosSystem {
         inherit system;
         modules = [
-          { nixpkgs.pkgs = args.pkgs; }
+          {
+            nixpkgs.pkgs = args.pkgs;
+            # FIXME: https://github.com/danth/stylix/issues/865
+            nixpkgs.overlays = lib.mkForce [];
+          }
           home-manager.nixosModules.home-manager
           stylix.nixosModules.stylix
           host
