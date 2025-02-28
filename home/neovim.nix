bb7f20bb069ad3610942ee90d5a71c6b344e125e
Shun Ueda <hasundue@gmail.com>
2025-02-28 15:35:01 +0900

    neovim: always use neovim from flake input

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,32 +1,18 @@
 { pkgs, ... }:
 
 let
-  nv = pkgs.writeShellScriptBin "nv" ''
-    if [ -d ~/nvim ]; then
-      exec nix run ~/nvim -- "$@"
-    else
-      exec nvim "$@"
-    fi
-  '';
+  neovim = pkgs.mkNeovim {
+    modules = pkgs.mkNeovim.modules.all;
+  };
 in
-
 {
   programs.git.extraConfig.core.editor = "nvim";
 
   home = {
-    packages = [
-      (pkgs.mkNeovim {
-        modules = with pkgs.mkNeovim.modules; [
-          core
-          clipboard
-          copilot
-          deno
-          nix
-          lua
-        ];
-      })
-      nv
-    ];
-    sessionVariables.EDITOR = "nv";
+    packages = [ neovim ];
+    shellAliases = {
+      nv = "nvim";
+    };
+    sessionVariables.EDITOR = "nvim";
   };
 }
