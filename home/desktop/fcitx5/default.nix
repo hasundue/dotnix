ac97372ea73f9328927dcdb754576493c986e5a0
Shun Ueda <hasundue@gmail.com>
2025-05-07 16:51:46 +0900

    fcitx5: declare configs

--- home/desktop/fcitx5/default.nix
+++ home/desktop/fcitx5/default.nix
@@ -1,24 +1,45 @@
 { pkgs, ... }:
 
 {
+  gtk = {
+    gtk2.extraConfig = ''
+      gtk-im-module="fcitx"
+    '';
+    gtk3.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+    gtk4.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+  };
+
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-catppuccin
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
+
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      settings = {
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "mozc";
+        };
+      };
+      waylandFrontend = true;
+    };
   };
 
   home.sessionVariables = {
-    GLFW_IM_MODULE = "ibus";
-    GTK_IM_MODULE = "fcitx";
     QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
-  };
-
-  xdg.configFile = {
-    "fcitx5/conf/classicui.conf".source = ./classicui.conf;
   };
 }
