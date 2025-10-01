d7cfc4bead65c5c65bbd13510d320ca7dc0f3b50
Shun Ueda <hasundue@gmail.com>
2025-10-01 14:08:21 +0900

    feat(lazygit): override revert command to follow conventional commits style

--- home/git.nix
+++ home/git.nix
@@ -25,7 +25,24 @@
 
   programs.lazygit = {
     enable = true;
-    settings.gui.sidePanelWidth = 0.5;
+    settings = {
+      gui = {
+        sidePanelWidth = 0.5;
+      };
+      keybinding = {
+        commits = {
+          revertCommit = "<disabled>";
+        };
+      };
+      customCommands = [
+        {
+          key = "t";
+          context = "commits";
+          command = "git revert {{.SelectedCommit.Sha}} --no-edit && git commit --amend -m \"revert: $(git log --format=%B -n 1 {{.SelectedCommit.Sha}} | head -1)\"";
+          description = "Revert";
+        }
+      ];
+    };
   };
 
   home = {
