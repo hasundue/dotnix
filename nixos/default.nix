36a6a7ff68b90192b7de5d3328fbb47ccb366f15
Shun Ueda <hasundue@gmail.com>
2025-10-18 22:02:29 +0900

    refactor(nixos): extract stylix config to separate module with darkened wallpaper

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,6 +6,10 @@
 }:
 
 {
+  imports = [
+    ./stylix.nix
+  ];
+
   i18n.defaultLocale = "en_US.UTF-8";
 
   environment = {
@@ -74,16 +78,6 @@
 
   services = {
     openssh.enable = true;
-  };
-
-  stylix = {
-    enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
-    image = pkgs.fetchurl {
-      url = "https://upload.wikimedia.org/wikipedia/commons/a/a5/Tsunami_by_hokusai_19th_century.jpg";
-      hash = "sha256-MMFwJg3jk/Ub1ZKtrMbwcUtg8VfAl0TpxbbUbmphNxg=";
-    };
-    polarity = "dark";
   };
 
   system = {
