8a162a12eab974bd576bb9daf0ae06ba50aec53e
Shun Ueda <hasundue@gmail.com>
2025-10-20 22:55:33 +0900

    niri: workaround screenshot binds

--- home/desktop/niri.nix
+++ home/desktop/niri.nix
@@ -311,8 +311,9 @@ in
 
       "Mod+W".action = toggle-column-tabbed-display;
 
-      "Print".action = screenshot;
-      "Alt+Print".action = screenshot-window;
+      "Print".action.screenshot = [ ];
+      "Ctrl+Print".action.screenshot-screen = [ ];
+      "Alt+Print".action.screenshot-window = [ ];
 
       "Mod+Escape" = {
         action = toggle-keyboard-shortcuts-inhibit;
