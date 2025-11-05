d1151ff948695ae1701b926d722c8de7d6f06a48
Shun Ueda <hasundue@gmail.com>
2025-11-05 21:33:45 +0900

    home: add hydra-check

--- home/nix.nix
+++ home/nix.nix
@@ -4,19 +4,19 @@
   home = {
     packages = with pkgs; [
       cachix
+      hydra-check
       nix-output-monitor
     ];
 
     shellAliases = rec {
-      nd = "nom develop";
+      hc = "hydra-check --short --channel nixos-unstable";
 
+      nd = "nom develop";
       nf = "nix flake";
       nfc = "${nf} check";
       nfs = "${nf} show";
       nfu = "${nf} update";
-
       nr = "nix run";
-
       ns = "nix search nixpkgs";
     };
   };
