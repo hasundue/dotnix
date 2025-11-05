449e70cc568fba93ab5e825153376a0e75efbbfb
Shun Ueda <hasundue@gmail.com>
2025-11-05 21:33:17 +0900

    nixos: make overlays-compat actually compatible with nix

--- nixos/_overlays-compat.nix
+++ nixos/_overlays-compat.nix
@@ -1,11 +1,7 @@
-[
-  (
-    final: prev:
-    with prev.lib;
-    let
-      flake = builtins.toString <nixos-config> |> builtins.getFlake;
-      inherit (flake.nixosConfigurations."@name@".config.nixpkgs) overlays;
-    in
-    foldl' (flip extends) (_: prev) overlays final
-  )
-]
+final: prev:
+with prev.lib;
+let
+  flake = builtins.toString <nixos-config> |> builtins.getFlake;
+  inherit (flake.nixosConfigurations."@name@".config.nixpkgs) overlays;
+in
+foldl' (flip extends) (_: prev) overlays final
