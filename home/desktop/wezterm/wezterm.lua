18463473332c6b430a2b8d3c2851ed9f3d731955
Shun Ueda <hasundue@gmail.com>
2025-11-05 22:04:43 +0900

    wezterm: configure font fallback

--- home/desktop/wezterm/wezterm.lua
+++ home/desktop/wezterm/wezterm.lua
@@ -4,4 +4,12 @@ local config = wezterm.config_builder()
 config.adjust_window_size_when_changing_font_size = false
 config.hide_tab_bar_if_only_one_tab = true
 
+config.font = wezterm.font_with_fallback({
+  "0xProto Nerd Font",
+  "Noto Sans Mono CJK JP",
+  "Noto Color Emoji",
+})
+
+config.use_cap_height_to_scale_fallback_fonts = true
+
 return config
