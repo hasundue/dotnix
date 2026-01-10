9c9e275b8209482a6817883bb1537a047cb147f1
Shun Ueda <hasundue@gmail.com>
2026-01-10 13:32:29 +0900

    chromium: optimize extensions

--- configs/home/desktop/chromium.nix
+++ configs/home/desktop/chromium.nix
@@ -5,8 +5,8 @@
       { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
       { id = "fihnjjcciajhdojfnbdddfaoknhalnja"; } # I don't care about cookies
-      { id = "kpgefcfmnafjgpblomihpgmejjdanjjp"; } # nos2x
-      { id = "fjdmkanbdloodhegphphhklnjfngoffa"; } # YouTube Auto HD
+      { id = "fcphghnknhkimeagdglkljinmpbagone"; } # YouTube Auto HD + FPS
+      { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite
     ];
   };
 }
