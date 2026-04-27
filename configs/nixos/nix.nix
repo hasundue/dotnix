0eea42dcccb17bc12386982aa05c2bc902ec42df
Shun Ueda <hasundue@gmail.com>
2026-04-27 11:34:48 +0900

    nixos/nix: add my own cachix

--- configs/nixos/nix.nix
+++ configs/nixos/nix.nix
@@ -39,10 +39,12 @@ in
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://hasundue.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hasundue.cachix.org-1:scWNpQXfdcSdrnNeZaoVJ9Cc41wLjKju/2fMANwUmtw="
       ];
       trusted-users = [ "@wheel" ];
     };
