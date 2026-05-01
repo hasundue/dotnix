b074cf5d317ce2123ba67299da649245de547abf
Shun Ueda <hasundue@gmail.com>
2026-05-01 11:29:48 +0900

    home/nix: make nix-output-monitor more verbose

--- configs/home/nix.nix
+++ configs/home/nix.nix
@@ -6,18 +6,23 @@
     hydra-check
     nix-output-monitor
   ];
-  home.shellAliases = rec {
-    hc = "hydra-check --short --channel nixos-unstable";
-    hm = "home-manager";
-    hms = "${hm} switch -b backup --flake . &| nom";
-    nd = "nom develop";
-    nf = "nix flake";
-    nfc = "${nf} check";
-    nfs = "${nf} show";
-    nfu = "${nf} update";
-    nor = "sudo nixos-rebuild";
-    nors = "${nor} switch --flake . &| nom";
-    nort = "${nor} test --flake . &| nom";
-    nr = "nix run";
-  };
+  home.shellAliases =
+    let
+      opts = "--log-format internal-json -v";
+      pipeToNom = ". &| nom --json";
+    in
+    rec {
+      hc = "hydra-check --short --channel nixos-unstable";
+      hm = "home-manager";
+      hms = "${hm} ${opts} switch -b backup --flake ${pipeToNom}";
+      nd = "nom develop";
+      nf = "nix flake";
+      nfc = "${nf} check";
+      nfs = "${nf} show";
+      nfu = "${nf} update";
+      nor = "sudo nixos-rebuild";
+      nors = "${nor} ${opts} switch --flake ${pipeToNom}";
+      nort = "${nor} ${opts} test --flake . ${pipeToNom}";
+      nr = "nix run";
+    };
 }
