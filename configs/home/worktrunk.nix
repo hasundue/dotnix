526ccca075f64cc3897f482c9432950abb0de091
Shun Ueda <hasundue@gmail.com>
2026-05-16 15:29:21 +0900

    worktrunk: default merge.remove to false

--- configs/home/worktrunk.nix
+++ configs/home/worktrunk.nix
@@ -2,6 +2,13 @@
 {
   home.packages = [ pkgs.worktrunk ];
 
+  home.file.".config/worktrunk/config.toml".text = ''
+    skip-shell-integration-prompt = true
+
+    [merge]
+    remove = false
+  '';
+
   programs.fish.interactiveShellInit = ''
     ${pkgs.worktrunk}/bin/wt config shell init fish | source
   '';
