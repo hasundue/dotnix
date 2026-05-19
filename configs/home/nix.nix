09b3599581a4d8bfcc5b6328200c74e52162a2d8
Shun Ueda <hasundue@gmail.com>
2026-05-19 21:02:38 +0900

    home/nix: restore shell aliases for nixos-rebuild commands

--- configs/home/nix.nix
+++ configs/home/nix.nix
@@ -2,17 +2,28 @@
 {
   home.packages = with pkgs; [
     cachix
+    home-manager
     hydra-check
     nix-output-monitor
   ];
   home.shellAliases =
+    let
+      opts = "--log-format internal-json -v";
+      pipeToNom = ". &| nom --json";
+    in
     rec {
       hc = "hydra-check --short --channel nixos-unstable";
+      hm = "home-manager";
+      hms = "${hm} ${opts} switch --flake ${pipeToNom}";
       nd = "nom develop";
       nf = "nix flake";
       nfc = "${nf} check";
       nfs = "${nf} show";
       nfu = "${nf} update";
+      nor = "sudo nixos-rebuild";
+      norb = "${nor} ${opts} boot --flake . ${pipeToNom}";
+      nors = "${nor} ${opts} switch --flake ${pipeToNom}";
+      nort = "${nor} ${opts} test --flake . ${pipeToNom}";
       nr = "nix run";
     };
 }
